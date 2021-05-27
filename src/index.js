de2bc868fc0a8e6feed522ce15dc939dc282d9b3
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-05-27 21:49:43 +0200

    basics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
 
+const counterReducer = (state = { value: 0 }, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        value: state.value + 1
+      }
+    case 'decrement':
+      return {
+        value: state.value - 1
+      }
+    default:
+      return state
+  }
+}
+
+let store = createStore(counterReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'increment' })
+// {value: 1}
+store.dispatch({ type: 'increment' })
+// {value: 2}
+store.dispatch({ type: 'decrement' })
+// {value: 1}
 ReactDOM.render(
   <React.StrictMode>
     <App />
